1e78c655a26a78e9a9ab29e4999dc046a1c13b7a
wingz92 <wingz92@yahoo.com>
2013-09-16 18:36:22 -0700

    Order Model

---
+++ HotelBookingSystem/OrderModel/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrderModel
+{
+    public class Order
+    {
+        public Order() 
+        { 
+        }
+        
+        public static string EncoderOrder(Order order)
+        {
+            return string.Empty;
+        }
+        
+        public static Order DecoderOrder(string encodedOrder)
+        {
+            return new Order();
+        }
+
+    }
+}

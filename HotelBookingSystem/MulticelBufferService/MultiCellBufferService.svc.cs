2911b7b5af8f55880466de23947952228df89489
kvv551 <victoria.kozlyuk@gmail.com>
2013-09-22 14:37:53 -0700

    Started working on MultiCellBuffer

--- HotelBookingSystem/MulticelBufferService/MultiCellBufferService.svc.cs
+++ HotelBookingSystem/MulticelBufferService/MultiCellBufferService.svc.cs
@@ -5,28 +5,23 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Threading;
 
 namespace MulticelBufferService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    public class Service1 : IMultiCellBufferService
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MulticelBufferService" in code, svc and config file together.
+    public class MulticelBufferService : IMultiCellBufferService
     {
-        public string GetData(int value)
+        private int bufferSize = 3;
+        private static Semaphore _sem = new Semaphore(0,3);
+
+        public void setOneCell(string encodedOrder)
         {
-            return string.Format("You entered: {0}", value);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public string getOneCell()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "";
         }
     }
 }

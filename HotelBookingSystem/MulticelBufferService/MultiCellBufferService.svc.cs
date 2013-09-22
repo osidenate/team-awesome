5976bd96b37f49dc710f38330aeb3ef01243a892
kvv551 <victoria.kozlyuk@gmail.com>
2013-09-22 15:51:43 -0700

    added setOneCell method

--- HotelBookingSystem/MulticelBufferService/MultiCellBufferService.svc.cs
+++ HotelBookingSystem/MulticelBufferService/MultiCellBufferService.svc.cs
@@ -12,11 +12,31 @@ namespace MulticelBufferService
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MulticelBufferService" in code, svc and config file together.
     public class MulticelBufferService : IMultiCellBufferService
     {
-        private int bufferSize = 3;
+        const int bufferSize = 3;
         private static Semaphore _sem = new Semaphore(0,3);
+        private string[] multiCellBuffer = new string[bufferSize];
 
         public void setOneCell(string encodedOrder)
         {
+            for (int i = 0; i < bufferSize; i++)
+            {
+                Monitor.Enter(multiCellBuffer[i]);
+                try
+                {
+                    if (multiCellBuffer[i].Length == 0)
+                        multiCellBuffer[i] = encodedOrder;
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    Monitor.Exit(multiCellBuffer[i]);
+                }
+
+                //_sem.Release(3);
+            }
+
         }
 
         public string getOneCell()
